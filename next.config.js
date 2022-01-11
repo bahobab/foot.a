9e31121ddf66f440143ff82e2a3eb283306397a4
Konan Houphoue <bahobab@hotmail.com>
2022-01-11 12:36:15 -0600

    Resolving cors issues: Set headers

--- next.config.js
+++ next.config.js
@@ -28,7 +28,6 @@ module.exports = {
       {
         // matching all API routes
         source: "/api/:path*",
-        // destination: "https://ba-footblog.vercel.app/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
--- pages/api/datocmsquery.js
+++ pages/api/datocmsquery.js
@@ -19,9 +19,14 @@ export default async function datocmsquery(req, res ) {
   const { query, variables, preview } = req.body;
 
   const respJSON = await fetchAPI(query, {variables, preview});
+
+  console.log('>>IN DATOCMS QUErY RESPONSE', respJSON);
   // if (req.method === "OPTIONS") {
-  //   res.setHeader("Access-Control-Allow-Origin", "*");
-  //   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
   //   return res.status(200).json({});
   // }
   return res.status(200).json(respJSON)
