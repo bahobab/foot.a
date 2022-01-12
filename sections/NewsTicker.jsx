4ca4461071372474eb5efbf81319c8f956f1c3c3
Konan Houphoue <bahobab@hotmail.com>
2022-01-12 02:45:30 -0600

    Ajust NewsTicker Marquee gradientWdth

--- sections/NewsTicker.jsx
+++ sections/NewsTicker.jsx
@@ -4,7 +4,7 @@ import Marquee from 'react-fast-marquee'
 function NewsTicker({children}) {
   return (
     <div id='news-ticker' className='bg-red-600 my-4'>
-      <Marquee pauseOnHover>
+      <Marquee pauseOnHover gradientWidth={80}>
         {children}
       </Marquee>
     </div>
